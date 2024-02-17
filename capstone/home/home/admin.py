53583e6adf83ade577194b13cc9d87b017890521
Kristoffer Krueger <kristoffer.krueger@bryanuniversity.edu>
2024-02-17 08:41:03 -0700

    week4

--- capstone/home/home/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import FeaturedItem
-
-# Register your models here.
-admin.site.register(FeaturedItem)
--- capstone/home/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import FeaturedItem
-
-def home(request):
-    featured_items = FeaturedItem.objects.all()
-
-    context = {'featured_items': featured_items}
-
-    return render(request, 'home.html', context)
